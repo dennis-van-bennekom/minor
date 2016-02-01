8200b390a801b9657f43ee2fd9499f7915083779
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2016-02-01 17:16:34 +0100

    Begin opdracht 5

---
+++ opdracht-5/static/js/app.js
@@ -0,0 +1,34 @@
+(function() {
+  'use strict';
+
+  var app = {
+    init: function() {
+      routes.init();
+    }
+  };
+
+  var routes = {
+    init: function() {
+      window.addEventListener('hashchange', function(event) {
+        var route = window.location.hash;
+
+        sections.toggle(route);
+      });
+    }
+  };
+
+  var sections = {
+    toggle: function(route) {
+      var sections = document.querySelectorAll('.section');
+
+      [].forEach.call(sections, function(section) {
+        section.classList.remove('active');
+      });
+
+      document.querySelector(route).classList.add('active');
+    }
+  }
+
+  app.init();
+
+})();
