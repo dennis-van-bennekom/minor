2a83d04f761f2386ee7ae5c9b8b1ec0588dc6769
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2016-02-09 10:20:52 +0100

    Opdracht 7 begin

--- opdracht-7/static/js/app.js
+++ opdracht-7/static/js/app.js
@@ -1,34 +1,81 @@
 (function () {
     'use strict';
 
-    var app = {
-        init: function () {
+    var $app = document.getElementById('app');
+    var $artistInput = document.getElementById('artist-input');
 
+    var app = {
+        init () {
             routes.init();
+            events.init();
+
+            if (window.location.hash === '') {
+                // Start with the home view
+                window.location.hash = 'home';
+            }
         }
     };
 
     var routes = {
-        init: function () {
+        init () {
+            routie({
+                'home': () => {
+                    views.render('home.mst');
+                },
 
-            window.addEventListener('hashchange', function (event) {
-                var route = window.location.hash;
+                ':artist': (artist) => {
+                    $artistInput.value = artist;
 
-                sections.toggle(route);
+                    var data = {
+                        artist: artist,
+                        loading: true
+                    };
+
+                    views.render('artist.mst', data);
+
+                    fetch(`https://ws.audioscrobbler.com/2.0/?api_key=9112859bf77f54259252dc9bc48a8cb9&method=artist.getSimilar&format=json&limit=12&autocorrect=1&artist=${artist}`)
+                        .then(response => response.json())
+                        .then(artists => {
+                            artists = artists.similarartists.artist;
+                            artists = _.sortBy(artists, 'name');
+
+                            console.log(artists);
+
+                            data = {
+                                artist: artist,
+                                artists: artists,
+                                loading: false
+                            };
+
+                            views.render('artist.mst', data);
+                        });
+                }
             });
         }
     };
 
-    var sections = {
-        toggle: function (route) {
+    var views = {
+        render (filename, data) {
+            var path = 'static/templates/' + filename;
+            data = data || {};
 
-            var sections = document.querySelectorAll('.section');
-
-            [].forEach.call(sections, function (section) {
-                section.classList.remove('active');
+            fetch(path).then(response => {
+                return response.text();
+            }).then(template => {
+                $app.innerHTML = Mustache.render(template, data);
             });
+        }
+    };
 
-            document.querySelector(route).classList.add('active');
+    var events = {
+        init () {
+            var searchForm = document.querySelector('.search-form');
+            searchForm.addEventListener('submit', event => {
+                event.preventDefault();
+
+                var artist = event.target.elements.artist.value;
+                routie(artist);
+            });
         }
     };
 
